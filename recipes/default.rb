c96b6ce4d481f3187a45c7dbec51f7ba8753147f
Darren Birkett <darren.birkett@gmail.com>
2013-01-18 16:35:49 +0000

    convert to array of roles

--- recipes/default.rb
+++ recipes/default.rb
@@ -31,7 +31,7 @@ ks_admin_endpoint = get_access_endpoint("keystone", "keystone", "admin-api")
 keystone = get_settings_by_role("keystone","keystone")
 
 node["ha"]["available_services"].each do |s|
-  role, ns, svc, svc_type = s["role"], s["namespace"], s["service"], s["service_type"]
+  roles, ns, svc, svc_type = s["roles"], s["namespace"], s["service"], s["service_type"]
 
   Chef::Log.info("Skipping: #{ns}-#{svc}") unless rcb_safe_deref(node, "vips.#{ns}-#{svc}")
 
@@ -40,10 +40,16 @@ node["ha"]["available_services"].each do |s|
     Chef::Log.info("Configuring virtual_server for #{ns}-#{svc}")
 
     # Lookup listen_port from the environment, or fall back to the first searched node running the role
-    listen_port = rcb_safe_deref(node, "#{ns}.services.#{svc}.port") ? node[ns]["services"][svc]["port"] : get_realserver_endpoints(role, ns, svc)[0]["port"]
+    listen_port = rcb_safe_deref(node, "#{ns}.services.#{svc}.port") ? node[ns]["services"][svc]["port"] : get_realserver_endpoints(roles[0], ns, svc)[0]["port"]
 
     # Generate array of host:port real servers
-    rs_list = get_realserver_endpoints(role, ns, svc).each.inject([]) { |output,x| output << {"ip" => x["host"], "port" => x["port"]} }
+#    rs_list = []
+#    roles.each do |role|
+#      (rs_list << get_realserver_endpoints(role, ns, svc).each.inject([]) { |output,x| output << {"ip" => x["host"], "port" => x["port"]} }).flatten!
+      (rs_list = roles.each.inject([]) {|me,r| me << get_realserver_endpoints(r, ns, svc).each.inject([]) { |output,x| output << {"ip" => x["host"], "port" => x["port"]} } }).flatten!
+#    end
+#    rs_list.flatten!
+    Chef::Log.info "rs_list is #{rs_list}"
 
     haproxy_virtual_server "#{ns}-#{svc}" do
       vs_listen_ip listen_ip
@@ -54,14 +60,14 @@ node["ha"]["available_services"].each do |s|
     # Need to update keystone endpoint
     case svc_type
     when "ec2"
-      public_endpoint = get_access_endpoint(role, ns, "ec2-public")
-      admin_path = get_settings_by_role(role, ns)['services']['ec2-admin']['path']
+      public_endpoint = get_access_endpoint(roles[0], ns, "ec2-public")
+      admin_path = get_settings_by_role(roles[0], ns)['services']['ec2-admin']['path']
       admin_endpoint = {'uri' => "#{public_endpoint['scheme']}://#{public_endpoint['host']}:#{public_endpoint['port']}#{admin_path}" }
     when "identity"
-      public_endpoint = get_access_endpoint(role, ns, "service-api")
-      admin_endpoint  = get_access_endpoint(role, ns, "admin-api")
+      public_endpoint = get_access_endpoint(roles[0], ns, "service-api")
+      admin_endpoint  = get_access_endpoint(roles[0], ns, "admin-api")
     else
-      public_endpoint = get_access_endpoint(role, ns, svc)
+      public_endpoint = get_access_endpoint(roles[0], ns, svc)
       admin_endpoint  = public_endpoint.clone
     end
 
