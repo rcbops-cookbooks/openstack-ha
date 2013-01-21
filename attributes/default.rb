cfd1af19554d91cffebccfda5e5c030abf14ce76
Darren Birkett <darren.birkett@gmail.com>
2013-01-21 15:32:41 +0000

    revert to single role per service

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,48 +1,48 @@
 default["ha"]["available_services"] = [
   {
-    "roles" => ["nova-api-os-compute"],
+    "role" => "nova-api-os-compute",
     "namespace" => "nova",
     "service" => "api",
     "service_type" => "compute"
   },
   {
-    "roles" => ["nova-api-ec2"],
+    "role" => "nova-api-ec2",
     "namespace" => "nova",
     "service" => "ec2-public",
     "service_type" => "ec2"
   },
   {
-    "roles" => ["keystone-api"],
+    "role" => "keystone-api",
     "namespace" => "keystone",
     "service" => "admin-api",
     "service_type" => "identity"
   },
   {
-    "roles" => ["keystone-api"],
+    "role" => "keystone-api",
     "namespace" => "keystone",
     "service" => "service-api",
     "service_type" => "identity"
   },
   {
-    "roles" => ["cinder-api"],
+    "role" => "cinder-api",
     "namespace" => "cinder",
     "service" => "api",
     "service_type" => "volume"
   },
   {
-    "roles" => ["glance-api"],
+    "role" => "glance-api",
     "namespace" => "glance",
     "service" => "api",
     "service_type" => "image"
   },
   {
-    "roles" => ["swift-proxy-server"],
+    "role" => "swift-proxy-server",
     "namespace" => "swift",
     "service" => "proxy",
     "service_type" => "object-store"
   },
   {
-    "roles" => ["glance-registry"],
+    "role" => "glance-registry",
     "namespace" => "glance",
     "service" => "registry",
     "service_type" => "image"
