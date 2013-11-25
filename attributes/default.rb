b88281c2c1ad5e140bee04520694eb6933e373b1
William Kelly <the.william.kelly@gmail.com>
2013-11-25 15:29:18 -0800

    Merge pull request #70 from Apsu/v4.2.1rc
    HA -- The Final Frontier

--- attributes/default.rb
+++ attributes/default.rb
@@ -8,6 +8,8 @@
 # default["vips"]["config"]["127.0.0.4"]["network"]="management"
 # default["vips"]["config"]["127.0.0.4"]["vrid"]=12
 
+default["apipa"] = "169.254.123.1"
+
 default["vips"]["mysql-db"] = nil
 default["vips"]["rabbitmq-queue"] = nil
 default["vips"]["horizon-dash"] = nil
